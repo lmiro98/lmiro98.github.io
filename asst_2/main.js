d6a2a47c5e9900dd8064bb333358ebeeec6e8467
Leon <l.miro98@gmail.com>
2020-04-02 22:18:16 -0400

    final entry

--- asst_2/main.js
+++ asst_2/main.js
@@ -4,10 +4,35 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Looping through images */
+/* Image handling */
+for(let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+      displayedImage.src = e.target.src;
+    }
+  }
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+/* Darken/Lighten button (Adapted from MDN Sample Site 
+    https://mdn.github.io/learning-area/javascript/building-blocks/gallery/) */
+function goDark(){
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+}
 
-/* Wiring up the Darken/Lighten button */
+function goLight(){
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+}
+
+btn.onclick = function() {
+  const btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    goDark();
+  } else {
+    goLight();
+  }
+}
